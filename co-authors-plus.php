Automattic/Co-Authors-Plus

commit 2ebbc3032112f5a84410ce6fecd7fd804bc38387

A new get_author_term() method to standardize how the author terms are accessed
See #65

co-authors-plus.php
@@ -297,7 +297,7 @@ class coauthors_plus {
 
 		// Update the taxonomy term to include details about the user for searching
 		$search_values = array();
-		$term = get_term_by( 'slug', $user_login, $this->coauthor_taxonomy );
+		$term = $this->get_author_term( $coauthor );
 		foreach( $this->ajax_search_fields as $search_field ) {
 			$search_values[] = $coauthor->$search_field;
 		}
@@ -601,12 +601,14 @@ class coauthors_plus {
 				$author_name = get_userdata( $query->get( 'author' ) )->user_nicename;
 
 			$terms = array();
-			if ( $author_term = get_term_by( 'slug', $author_name, $this->coauthor_taxonomy ) )
-				$terms[] = $author_term;
 			$coauthor = $this->get_coauthor_by( 'user_nicename', $author_name );
+			if ( $author_term = $this->get_author_term( $coauthor ) )
+				$terms[] = $author_term;
 			// If this coauthor has a linked account, we also need to get posts with those terms
-			if ( !empty( $coauthor->linked_account ) && $guest_author_term = get_term_by( 'slug', $author_name, $this->coauthor_taxonomy ) ) {
-				$terms[] = $guest_author_term;
+			if ( ! empty( $coauthor->linked_account ) ) {
+				$linked_author = get_user_by( 'login', $coauthor->linked_author );
+				if ( $guest_author_term = $this->get_author_term( $linked_author ) )
+					$terms[] = $guest_author_term;
 			}
 
 			// Whether or not to include the original 'post_author' value in the query
@@ -822,8 +824,7 @@ class coauthors_plus {
 	function filter_count_user_posts( $count, $user_id ) {
 		$user = get_userdata( $user_id );
 
-		$term = get_term_by( 'slug', $user->user_nicename, $this->coauthor_taxonomy );
-
+		$term = $this->get_author_term( $user );
 		// Only modify the count if the author already exists as a term
 		if( $term && !is_wp_error( $term ) ) {
 			$count = $term->count;
@@ -931,7 +932,7 @@ class coauthors_plus {
 		remove_filter( 'pre_user_query', array( $this, 'filter_pre_user_query' ) );
 
 		foreach( $found_users as $found_user ) {
-			$term = get_term_by( 'slug', $found_user->user_nicename, $this->coauthor_taxonomy );
+			$term = $this->get_author_term( $found_user );
 			if ( empty( $term ) || empty( $term->description ) ) {
 				$this->refresh_coauthor_term( $found_user->user_login );
 			}
@@ -1113,6 +1114,32 @@ class coauthors_plus {
 		}
 
 		return $allcaps;
+	}
+
+	/**
+	 * Get the author term for a given co-author
+	 *
+	 * @since 0.7
+	 *
+	 * @param object $coauthor The co-author object
+	 * @return object|false $author_term The author term on success
+	 */
+	public function get_author_term( $coauthor ) {
+
+		if ( ! is_object( $coauthor ) )
+			return;
+
+		$cache_key = 'author-term-' . $coauthor->user_nicename;
+		if ( false !== ( $term = wp_cache_get( $cache_key, 'co-authors-plus' ) ) )
+			return $term;
+
+		// See if the prefixed term is available, otherwise default to just the nicename
+		$term = get_term_by( 'slug', 'cap-' . $coauthor->user_nicename, $this->coauthor_taxonomy );
+		if ( ! $term ) {
+			$term = get_term_by( 'slug', $coauthor->user_nicename, $this->coauthor_taxonomy );
+		}
+		wp_cache_set( $cache_key, $term, 'co-authors-plus' );
+		return $term;
 	}
 
 	/**

php/class-coauthors-guest-authors.php
@@ -199,18 +199,17 @@ class CoAuthors_Guest_Authors
 			wp_die( __( "Guest author can't be deleted because it doesn't exist.", 'co-authors-plus' ) );
 
 		// Perform the reassignment if needed
+		$guest_author_term = $coauthors_plus->get_author_term( $guest_author );
 		switch( $_POST['reassign'] ) {
 			// Leave assigned to the current linked account
 			case 'leave-assigned':
-				$guest_author_term = get_term_by( 'slug', $guest_author->user_login, $coauthors_plus->coauthor_taxonomy );
-				$linked_account_term = get_term_by( 'slug', $guest_author->linked_account, $coauthors_plus->coauthor_taxonomy );
+				$linked_account_term = $coauthors_plus->get_author_term( get_user_by( 'login', $guest_author->linked_account ) );
 				// If they aren't the same, delete the guest author term and reassign those to the linked account term
 				if ( $guest_author_term->term_id != $linked_account_term->term_id )
 					wp_delete_term( $guest_author_term->term_id, $coauthors_plus->coauthor_taxonomy, array( 'default' => $linked_account_term->term_id, 'force_default' => true ) );
 				break;
 			// Remove the byline, but don't delete the post
 			case 'remove-byline':
-				$guest_author_term = get_term_by( 'slug', $guest_author->user_login, $coauthors_plus->coauthor_taxonomy );
 				wp_delete_term( $guest_author_term->term_id, $coauthors_plus->coauthor_taxonomy );
 				break;
 			default:
@@ -556,7 +555,7 @@ class CoAuthors_Guest_Authors
 		// Update the taxonomy term to include details about the user for searching
 		$search_values = array();
 		$guest_author = $this->get_guest_author_by( 'id', $post_id );
-		$term = get_term_by( 'slug', $guest_author->user_login, $coauthors_plus->coauthor_taxonomy );
+		$term = $coauthors_plus->get_author_term( $guest_author );
 		foreach( $coauthors_plus->ajax_search_fields as $search_field ) {
 			$search_values[] = $guest_author->$search_field;
 		}

php/class-coauthors-wp-list-table.php
@@ -215,7 +215,7 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 	 */
 	function column_posts( $item ) {
 		global $coauthors_plus;
-		$term = get_term_by( 'name', $item->user_login, $coauthors_plus->coauthor_taxonomy );
+		$term = $coauthors_plus->get_author_term( $item );
 		if ( ! $term )
 			return '';
 		return '<a href="' . esc_url( add_query_arg( 'author_name', $item->user_login, admin_url( 'edit.php' ) ) ) . '">' . $term->count . '</a>';

php/class-wp-cli.php
@@ -200,7 +200,7 @@ EOB
 				$new_user = get_user_by( 'id', $new_user )->user_login;
 
 			// The old user should exist as a term
-			$old_term = get_term_by( 'slug', $old_user, $coauthors_plus->coauthor_taxonomy );
+			$old_term = $coauthors_plus->get_author_term( get_user_by( 'login', $old_user ) );
 			if ( !$old_term ) {
 				WP_CLI::line( "Error: Term '{$old_user}' doesn't exist, skipping" );
 				$results->old_term_missing++;
@@ -210,7 +210,7 @@ EOB
 			// If the new user exists as a term already, we want to reassign all posts to that
 			// new term and delete the original
 			// Otherwise, simply rename the old term
-			$new_term = get_term_by( 'slug', $new_user, $coauthors_plus->coauthor_taxonomy );
+			$new_term = $coauthors_plus->get_author_term( get_user_by( 'login', $new_user ) );
 			if ( is_object( $new_term ) ) {
 				$args = array(
 						'default' => $new_term->term_id,

commit f70c40720e6b64bbe9878638cdfd883dd36fb3a7

s/linked_author/linked_account/
Typo introduced in 2ebbc3032112f5a84410ce6fecd7fd804bc38387

co-authors-plus.php
@@ -568,8 +568,8 @@ class coauthors_plus {
 				$terms[] = $author_term;
 			// If this coauthor has a linked account, we also need to get posts with those terms
 			if ( ! empty( $coauthor->linked_account ) ) {
-				$linked_author = get_user_by( 'login', $coauthor->linked_author );
-				if ( $guest_author_term = $this->get_author_term( $linked_author ) )
+				$linked_account = get_user_by( 'login', $coauthor->linked_account );
+				if ( $guest_author_term = $this->get_author_term( $linked_account ) )
 					$terms[] = $guest_author_term;
 			}
 