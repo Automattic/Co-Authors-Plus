6f107f7376785c6b1762fc0dbae31576c3eebda0
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-01-10 10:49:06 -0500

    Providing arguments for optional span class
    As automatically adding the span class might break existing themes,
    moved this implementation to make it optional and applied through
    filter, while retaining the in-link class.
    
    I'm unsure of coding standards for this setup, but I figured that the
    bracketing variables should be added after the sprintf and it made sense
    to break down the return into two steps. Open to a better way of doing
    this if there is one.

--- template-tags.php
+++ template-tags.php
@@ -227,23 +227,24 @@ function coauthors_posts_links( $between = null, $betweenLast = null, $before = 
  */
 function coauthors_posts_links_single( $author ) {
 	$args = array(
-		'span_class' => 'author vcard',
+		'before_html' => '',
 		'href' => get_author_posts_url( $author->ID, $author->user_nicename ),
 		'rel' => 'author',
 		'title' => sprintf( __( 'Posts by %s', 'co-authors-plus' ), get_the_author() ),
-		'link_class' => 'url fn',
+		'class' => 'url fn',
 		'text' => get_the_author(),
+		'after_html' => ''
 	);
 	$args = apply_filters( 'coauthors_posts_link', $args, $author );
-	return sprintf(
-			'<span class="%1$s"><a href="%2$s" title="%3$s" class="%4$s" rel="%5$s">%6$s</a></span>',
-			esc_attr( $args['span_class'] ),
+	$single_link = sprintf(
+			'<a href="%1$s" title="%2$s" class="%3$s" rel="%4$s">%5$s</a>',
 			esc_url( $args['href'] ),
 			esc_attr( $args['title'] ),
-			esc_attr( $args['link_class'] ),
+			esc_attr( $args['class'] ),
 			esc_attr( $args['rel'] ),
 			esc_html( $args['text'] )
 	);
+	return $args['before_html'] . $single_link . $args['after_html'];
 }
 
 /**
