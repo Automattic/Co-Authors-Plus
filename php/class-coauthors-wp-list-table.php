030732efd3f7b6f25f89174340bf37c88a31e206
Paul Schreiber <paul.schreiber@espn.com>
2014-09-08 12:11:01 -0400

    fix PHPCS errors in class-coauthors-wp-list-table.php

--- php/class-coauthors-wp-list-table.php
+++ php/class-coauthors-wp-list-table.php
@@ -12,8 +12,9 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 	var $is_search = false;
 
 	function __construct() {
-		if( !empty( $_REQUEST['s'] ) )
+		if ( ! empty( $_REQUEST['s'] ) ) {
 			$this->is_search = true;
+		}
 
 		parent::__construct( array(
 				'plural' => __( 'Co-Authors', 'co-authors-plus' ),
@@ -34,17 +35,19 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 				'first_name'         => array( 'first_name', 'ASC' ),
 				'last_name'          => array( 'last_name', 'ASC' ),
 			);
-		$_sortable = apply_filters( "coauthors_guest_author_sortable_columns", $this->get_sortable_columns() );
+		$_sortable = apply_filters( 'coauthors_guest_author_sortable_columns', $this->get_sortable_columns() );
 
-		foreach( (array)$_sortable as $id => $data ) {
-			if ( empty( $data ) )
+		foreach ( (array)$_sortable as $id => $data ) {
+			if ( empty( $data ) ) {
 				continue;
+			}
 
 			$data = (array) $data;
-			if ( !isset( $data[1] ) )
+			if ( ! isset( $data[1] ) ) {
 				$data[1] = false;
+			}
 
-			$sortable[$id] = $data;
+			$sortable[ $id ] = $data;
 		}
 
 		$this->_column_headers = array( $columns, $hidden, $sortable );
@@ -62,7 +65,7 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 			);
 
 		if ( isset( $_REQUEST['orderby'] ) ) {
-			switch( $_REQUEST['orderby'] ) {
+			switch ( $_REQUEST['orderby'] ) {
 				case 'display_name':
 					$args['orderby'] = 'title';
 					break;
@@ -89,29 +92,32 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 			$this->active_filter = 'show-all';
 		}
 
-		switch( $this->active_filter ) {
+		switch ( $this->active_filter ) {
 			case 'with-linked-account':
 			case 'without-linked-account':
 				$args['meta_key'] = $coauthors_plus->guest_authors->get_post_meta_key( 'linked_account' );
-				if ( 'with-linked-account' == $this->active_filter )
+				if ( 'with-linked-account' == $this->active_filter ) {
 					$args['meta_compare'] = '!=';
-				else
+				} else {
 					$args['meta_compare'] = '=';
+				}
 				$args['meta_value'] = '0';
 				break;
 		}
 
-		if( $this->is_search )
+		if ( $this->is_search ) {
 			add_filter( 'posts_where', array( $this, 'filter_query_for_search' ) );
+		}
 
 		$author_posts = new WP_Query( $args );
 		$items = array();
-		foreach( $author_posts->get_posts() as $author_post ) {
+		foreach ( $author_posts->get_posts() as $author_post ) {
 			$items[] = $coauthors_plus->guest_authors->get_guest_author_by( 'ID', $author_post->ID );
 		}
 
-		if( $this->is_search )
+		if ( $this->is_search ) {
 			remove_filter( 'posts_where', array( $this, 'filter_query_for_search' ) );
+		}
 
 		$this->items = $items;
 
@@ -124,7 +130,7 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 	function filter_query_for_search( $where ) {
 		global $wpdb;
 		$var = '%' . sanitize_text_field( $_REQUEST['s'] ) . '%';
-		$where .= $wpdb->prepare( ' AND (post_title LIKE %s OR post_name LIKE %s )', $var, $var);
+		$where .= $wpdb->prepare( ' AND (post_title LIKE %s OR post_name LIKE %s )', $var, $var );
 		return $where;
 	}
 
@@ -148,7 +154,7 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 				'posts'          => __( 'Posts', 'co-authors-plus' ),
 			);
 
-		$columns = apply_filters( "coauthors_guest_author_manage_columns", $columns );
+		$columns = apply_filters( 'coauthors_guest_author_manage_columns', $columns );
 		return $columns;
 	}
 
@@ -160,8 +166,8 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 		$alternate_class = ( $alternate_class == '' ? ' alternate' : '' );
 		$row_class = ' class="guest-author-static' . $alternate_class . '"';
 
-		echo '<tr id="guest-author-' . $item->ID . '"' . $row_class . '>';
-		echo $this->single_row_columns( $item );
+		echo '<tr id="guest-author-' . esc_attr( $item->ID ) . '"' . $row_class . '>';
+		echo esc_html( $this->single_row_columns( $item ) );
 		echo '</tr>';
 	}
 
@@ -170,7 +176,7 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 	 */
 	function column_default( $item, $column_name ) {
 
-		switch( $column_name ) {
+		switch ( $column_name ) {
 			case 'first_name':
 			case 'last_name':
 				return $item->$column_name;
@@ -178,7 +184,7 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 				return '<a href="' . esc_attr( 'mailto:' . $item->user_email ) . '">' . esc_html( $item->user_email ) . '</a>';
 
 			default:
-				do_action( "coauthors_guest_author_custom_columns", $column_name, $item->ID );
+				do_action( 'coauthors_guest_author_custom_columns', $column_name, $item->ID );
 			break;
 		}
 	}
@@ -200,7 +206,7 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 		$output = '';
 
 		$output .= coauthors_get_avatar( $item, 32 );
-		
+
 		if ( current_user_can( 'edit_post', $item->ID ) ) {
 			$output .= '<a href="' . esc_url( $item_edit_link ) . '">' . esc_html( $item->display_name ) . '</a>';
 		} else {
@@ -243,10 +249,11 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 	function column_posts( $item ) {
 		global $coauthors_plus;
 		$term = $coauthors_plus->get_author_term( $item );
-		if ( $term )
+		if ( $term ) {
 			$count = $term->count;
-		else
+		} else {
 			$count = 0;
+		}
 		return '<a href="' . esc_url( add_query_arg( 'author_name', $item->user_login, admin_url( 'edit.php' ) ) ) . '">' . $count . '</a>';
 	}
 
@@ -257,9 +264,9 @@ class CoAuthors_WP_List_Table extends WP_List_Table {
 
 		?><div class="alignleft actions"><?php
 		if ( 'top' == $which ) {
-			if ( !empty( $this->filters ) ) {
+			if ( ! empty( $this->filters ) ) {
 				echo '<select name="filter">';
-				foreach( $this->filters as $key => $value ) {
+				foreach ( $this->filters as $key => $value ) {
 					echo '<option value="' . esc_attr( $key ) . '" ' . selected( $this->active_filter, $key, false ) . '>' . esc_attr( $value ) . '</option>';
 				}
 				echo '</select>';
