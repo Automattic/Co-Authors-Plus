ea2f3afde5c20ea81884721196d16e25d0f5bb35
Daniel Bachhuber <daniel.bachhuber@fusion.net>
2015-09-15 13:58:59 -0700

    Use WP 4.3 tagged release for WP develop checkout
    This will help avoid getting bitten by breaking changes https://core.trac.wordpress.org/ticket/33888

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -42,9 +42,9 @@ install_test_suite() {
 	# set up testing suite
 	mkdir -p $WP_TESTS_DIR
 	cd $WP_TESTS_DIR
-	svn co --quiet http://develop.svn.wordpress.org/trunk/tests/phpunit/includes/
+	svn co --quiet http://develop.svn.wordpress.org/tags/4.3/tests/phpunit/includes/
 
-	wget -nv -O wp-tests-config.php http://develop.svn.wordpress.org/trunk/wp-tests-config-sample.php
+	wget -nv -O wp-tests-config.php http://develop.svn.wordpress.org/tags/4.3/wp-tests-config-sample.php
 	sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" wp-tests-config.php
 	sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" wp-tests-config.php
 	sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
