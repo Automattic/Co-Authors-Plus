356a30c938ed0dc8d9c6b5c12176e579c5912651
Paul Schreiber <paul.schreiber@espn.com>
2015-09-16 21:40:21 -0400

    single quote strings

--- co-authors-plus.php
+++ co-authors-plus.php
@@ -618,10 +618,10 @@ class coauthors_plus {
 			$term_taxonomy_join = " INNER JOIN {$wpdb->term_taxonomy} ON ( {$wpdb->term_relationships}.term_taxonomy_id = {$wpdb->term_taxonomy}.term_taxonomy_id )";
 
 			if ( false === strpos( $join, trim( $term_relationship_join ) ) ) {
-				$join .= str_replace( "INNER JOIN", "LEFT JOIN", $term_relationship_join );
+				$join .= str_replace( 'INNER JOIN', 'LEFT JOIN', $term_relationship_join );
 			}
 			if ( false === strpos( $join, trim( $term_taxonomy_join ) ) ) {
-				$join .= str_replace( "INNER JOIN", "LEFT JOIN", $term_taxonomy_join );
+				$join .= str_replace( 'INNER JOIN', 'LEFT JOIN', $term_taxonomy_join );
 			}
 		}
 
