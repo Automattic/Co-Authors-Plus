f82cf0ab53141c8fd7115567550280e99c39f88b
Paul Schreiber <paul.schreiber@espn.com>
2015-09-16 21:39:42 -0400

    remove commented-out and unreachable code

--- upgrade.php
+++ upgrade.php
@@ -34,15 +34,12 @@ function coauthors_plus_upgrade_20() {
 			$legacy_coauthors = get_post_meta( $single_post->ID, '_coauthor' );
 
 			if ( is_array( $legacy_coauthors ) ) {
-				//echo '<p>Has Legacy coauthors';
 				foreach ( $legacy_coauthors as $legacy_coauthor ) {
 					$legacy_coauthor_login = get_user_by( 'id', (int) $legacy_coauthor );
 					if ( is_object( $legacy_coauthor_login ) && ! in_array( $legacy_coauthor_login->user_login, $coauthors ) ) {
 						$coauthors[] = $legacy_coauthor_login->user_login;
 					}
 				}
-			} else {
-				// No Legacy coauthors
 			}
 			$coauthors_plus->add_coauthors( $single_post->ID, $coauthors );
 
