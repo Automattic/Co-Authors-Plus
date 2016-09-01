ba23fb5a6a7611ca7067aa18667e1cc095f199aa
Mohammad Jangda <batmoo@gmail.com>
2016-09-01 19:02:48 -0400

    Add some basic WP_Query tests
    We can use these to verify that our author query mods work

--- tests/coauthorsplus-testcase.php
+++ tests/coauthorsplus-testcase.php
@@ -3,4 +3,11 @@
 /**
  * Base unit test class for Co-Authors Plus
  */
-class CoAuthorsPlus_TestCase extends WP_UnitTestCase {}
+class CoAuthorsPlus_TestCase extends WP_UnitTestCase {
+	public function setUp() {
+		parent::setUp();
+
+		global $coauthors_plus;
+		$this->_cap = $coauthors_plus;
+	}
+}
