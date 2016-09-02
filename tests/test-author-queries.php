b81e28392eb5c24f8efdfc7c937935fb50e05499
Mohammad Jangda <batmoo@gmail.com>
2016-09-01 22:50:09 -0400

    Call `add_coauthors` for WP_Query tests
    And add an additional test for post_author + tax query

--- tests/test-author-queries.php
+++ tests/test-author-queries.php
@@ -4,11 +4,13 @@ class Test_Author_Queries extends CoAuthorsPlus_TestCase {
 
 	public function test__author_arg__user_is_post_author() {
 		$author_id = $this->factory->user->create( array( 'role' => 'author', 'user_login' => 'batman' ) );
+		$author = get_userdata( $author_id );
 		$post_id = $this->factory->post->create( array(
 			'post_author'     => $author_id,
 			'post_status'     => 'publish',
 			'post_type'       => 'post',
 		) );
+		$this->_cap->add_coauthors( $post_id, array( $author->user_login ) );
 
 		$query = new WP_Query( array(
 			'author' => $author_id,
@@ -26,6 +28,7 @@ class Test_Author_Queries extends CoAuthorsPlus_TestCase {
 			'post_status'     => 'publish',
 			'post_type'       => 'post',
 		) );
+		$this->_cap->add_coauthors( $post_id, array( $author->user_login ) );
 
 		$query = new WP_Query( array(
 			'author_name' => $author->user_login,
@@ -34,7 +37,6 @@ class Test_Author_Queries extends CoAuthorsPlus_TestCase {
 		$this->assertEquals( 1, count( $query->posts ) );
 		$this->assertEquals( $post_id, $query->posts[ 0 ]->ID );
 	}
-
 
 	public function test__author_name_arg__user_is_coauthor() {
 		$author1_id = $this->factory->user->create( array( 'role' => 'author', 'user_login' => 'batman' ) );
@@ -74,6 +76,26 @@ class Test_Author_Queries extends CoAuthorsPlus_TestCase {
 
 		$query = new WP_Query( array(
 			'author' => $author2_id,
+		) );
+
+		$this->assertEquals( 1, count( $query->posts ) );
+		$this->assertEquals( $post_id, $query->posts[ 0 ]->ID );
+	}
+
+	public function test__author_name_arg_plus_tax_query__user_is_post_author() {
+		$author_id = $this->factory->user->create( array( 'role' => 'author', 'user_login' => 'batman' ) );
+		$author = get_userdata( $author_id );
+		$post_id = $this->factory->post->create( array(
+			'post_author'     => $author_id,
+			'post_status'     => 'publish',
+			'post_type'       => 'post',
+		) );
+		$this->_cap->add_coauthors( $post_id, array( $author->user_login ) );
+		wp_set_post_terms( $post_id, 'test', 'post_tag' );
+
+		$query = new WP_Query( array(
+			'author_name' => $author->user_login,
+			'tag' => 'test',
 		) );
 
 		$this->assertEquals( 1, count( $query->posts ) );
