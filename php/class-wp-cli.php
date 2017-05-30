68bcb6e4b7f905ef5276c92f1e5dbc532787a915
Rebecca Hum <rhum1@studentmail.nait.ca>
2017-05-30 12:18:20 -0600

    Fixed WP CLI create-terms-for-posts if no co-authors found
    Script was stopping after no co-authors was find. Now script will
    display an error message and continue to the next post.

--- php/class-wp-cli.php
+++ php/class-wp-cli.php
@@ -78,7 +78,8 @@ class CoAuthorsPlus_Command extends WP_CLI_Command {
 
 				$terms = cap_get_coauthor_terms_for_post( $single_post->ID );
 				if ( empty( $terms ) ) {
-					WP_CLI::error( sprintf( 'No co-authors found for post #%d.', $single_post->ID ) );
+					WP_CLI::line( sprintf( 'No co-authors found for post #%d.', $single_post->ID ) );
+					continue;
 				}
 
 				if ( ! empty( $terms ) ) {
