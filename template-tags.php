c4624caa88ba5e47e069c0b3ae86c51c5122dd03
rebeccahum <rhum1@studentmail.nait.ca>
2017-10-13 11:27:32 -0600

    Adding escaping

--- template-tags.php
+++ template-tags.php
@@ -381,21 +381,21 @@ function coauthors_links_single( $author ) {
 	if ( 'guest-author' === $author->type ) {
 		if ( get_the_author_meta( 'website' ) ) {
 			return sprintf( '<a href="%s" title="%s" rel="external" target="_blank">%s</a>',
-				get_the_author_meta( 'website' ),
-				esc_attr( sprintf( __( 'Visit %s&#8217;s website' ), get_the_author() ) ),
-				get_the_author()
+				esc_url( get_the_author_meta( 'website' ) ),
+				esc_attr( sprintf( __( 'Visit %s&#8217;s website' ), esc_html( get_the_author() ) ) ),
+				esc_html( get_the_author() )
 			);
 		} 
 	}
 	elseif ( get_the_author_meta( 'url' ) ) {
 		return sprintf( '<a href="%s" title="%s" rel="external" target="_blank">%s</a>',
-			get_the_author_meta( 'url' ),
-			esc_attr( sprintf( __( 'Visit %s&#8217;s website' ), get_the_author() ) ),
-			get_the_author()
+			esc_url( get_the_author_meta( 'url' ) ),
+			esc_attr( sprintf( __( 'Visit %s&#8217;s website' ), esc_html( get_the_author() ) ) ),
+			esc_html( get_the_author() )
 		);
 	} 
 	else {
-		return get_the_author();
+		return esc_html( get_the_author() );
 	}
 }
 
