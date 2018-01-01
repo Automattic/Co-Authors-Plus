7148d8d36fe2eac242f77737d9c0fdb20610addf
sanketio <sanket.parmar11@gmail.com>
2018-01-01 19:37:30 +0530

    [#184] Unit tests for coauthors orders

--- tests/test-template-tags.php
+++ tests/test-template-tags.php
@@ -87,6 +87,39 @@ class Test_Template_Tags extends CoAuthorsPlus_TestCase {
 	}
 
 	/**
+	 * Checks coauthors order.
+	 *
+	 * @covers ::get_coauthors()
+	 */
+	public function test_coauthors_order() {
+
+		global $coauthors_plus;
+
+		$post_id = $this->factory->post->create();
+
+		// Checks when no author exist.
+		$this->assertEmpty( get_coauthors( $post_id ) );
+
+		// Checks coauthors order.
+		$coauthors_plus->add_coauthors( $post_id, array( $this->author1->user_login ), true );
+		$coauthors_plus->add_coauthors( $post_id, array( $this->editor1->user_login ), true );
+
+		$expected = array( $this->author1->user_login, $this->editor1->user_login );
+
+		$this->assertEquals( $expected, wp_list_pluck( get_coauthors( $post_id ), 'user_login' ) );
+
+		// Checks coauthors order after modifying.
+		$post_id = $this->factory->post->create();
+
+		$coauthors_plus->add_coauthors( $post_id, array( $this->editor1->user_login ), true );
+		$coauthors_plus->add_coauthors( $post_id, array( $this->author1->user_login ), true );
+
+		$expected = array( $this->editor1->user_login, $this->author1->user_login );
+
+		$this->assertEquals( $expected, wp_list_pluck( get_coauthors( $post_id ), 'user_login' ) );
+	}
+
+	/**
 	 * Checks whether user is a coauthor of the post when user or post not exists.
 	 *
 	 * @covers ::is_coauthor_for_post()
