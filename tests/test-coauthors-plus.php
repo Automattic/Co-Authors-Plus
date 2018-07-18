04750c42e35fb35a2492afb811c87ca7d27558bc
Stefano Ottolenghi <stejey@gmail.com>
2018-07-18 09:12:25 +0100

    $ignored_authors in search should use user_nicename, not login.

--- tests/test-coauthors-plus.php
+++ tests/test-coauthors-plus.php
@@ -442,7 +442,7 @@ class Test_CoAuthors_Plus extends CoAuthorsPlus_TestCase {
 		global $coauthors_plus;
 
 		// Ignoring single author.
-		$ignored_authors = array( $this->author1->user_login );
+		$ignored_authors = array( $this->author1->user_nicename );
 
 		$authors = $coauthors_plus->search_authors( '', $ignored_authors );
 
@@ -461,7 +461,7 @@ class Test_CoAuthors_Plus extends CoAuthorsPlus_TestCase {
 		$this->assertArrayHasKey( $author2->user_login, $authors );
 
 		// Ignoring multiple authors.
-		$authors = $coauthors_plus->search_authors( '', array( $this->author1->user_login, $author2->user_login ) );
+		$authors = $coauthors_plus->search_authors( '', array( $this->author1->user_nicename, $author2->user_nicename ) );
 
 		$this->assertNotEmpty( $authors );
 		$this->assertArrayNotHasKey( $this->author1->user_login, $authors );
@@ -478,7 +478,7 @@ class Test_CoAuthors_Plus extends CoAuthorsPlus_TestCase {
 		global $coauthors_plus;
 
 		// Checks when ignoring author1.
-		$ignored_authors = array( $this->author1->user_login );
+		$ignored_authors = array( $this->author1->user_nicename );
 
 		$this->assertEmpty( $coauthors_plus->search_authors( $this->author1->ID, $ignored_authors ) );
 
