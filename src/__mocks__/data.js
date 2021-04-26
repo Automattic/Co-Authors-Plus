4cd0d39577167afbe41fd7defc8f22627711611b
laras126 <laras126@gmail.com>
2021-04-26 09:53:16 -0400

    test: move fixtures

--- src/__mocks__/data.js
+++ src/__mocks__/data.js
@@ -3,15 +3,18 @@ export const optionsMock = [
 		name: 'tester',
 		value: 'cap-pmcdev',
 		label: 'tester local@test.com something else',
+		order: 1
 	},
 	{
 		name: 'guest',
 		value: 'cap-guest',
 		label: 'guest local@test.com something else',
+		order: 0
 	},
 	{
 		name: 'demo',
 		value: 'cap-demo',
 		label: 'demo local@test.com something else',
+		order: 2
 	},
 ];
