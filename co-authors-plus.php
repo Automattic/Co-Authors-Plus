c2181ce28d9d769f298c9c13b13ae4a553475c26
Maximo Martinez Soria <43972710+maximomartinezsoria@users.noreply.github.com>
2022-10-19 10:36:03 -0300

    Remove pre_handle_404 filter
    Problem: non-existent paged author archives are not throwing a 404. For example: /author/slug/page/100000. Will load and be indexed by crawlers.
    
    Others are having this problem too: https://github.com/Automattic/Co-Authors-Plus/issues/703

--- co-authors-plus.php
+++ co-authors-plus.php
@@ -1209,7 +1209,6 @@ class CoAuthors_Plus {
 		if ( is_object( $authordata ) || ! empty( $term ) ) {
 			$wp_query->queried_object    = $authordata;
 			$wp_query->queried_object_id = $authordata->ID;
-			add_filter( 'pre_handle_404', '__return_true' );
 		} else {
 			$wp_query->queried_object = $wp_query->queried_object_id = null;
 			$wp_query->is_author      = $wp_query->is_archive = false;
