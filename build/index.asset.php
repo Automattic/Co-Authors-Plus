bd01d451f91978a1207fb0876dc6b4e1a5bfee99
Jo Murgel <dev@jomurgel.com>
2022-11-02 08:57:46 -0600

    rebuilds

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '416678425e551e71c404');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'bb6ab69775bc015b8da7');
