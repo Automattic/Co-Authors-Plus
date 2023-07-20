e9d4109b8ec8c25f4103537bf2f8960b5e8f2c7e
Douglas Johnson <doug@thoughtcatalog.com>
2023-07-20 10:45:47 -0400

    output link in display name block render callback

--- blocks/coauthor-display-name/class-cap-block-coauthor-display-name.php
+++ blocks/coauthor-display-name/class-cap-block-coauthor-display-name.php
@@ -35,11 +35,29 @@ class CAP_Block_CoAuthor_Display_Name {
 	 * @return string
 	 */
 	public static function render_block( array $attributes, string $content, WP_Block $block ) : string {
-		$display_name = array_key_exists( 'display_name', $block->context ) ? $block->context['display_name'] : '';
+
+		$display_name = $block->context['display_name'] ?? '';
+
 		if ( '' === $display_name ) {
 			return '';
 		}
-		$attributes = get_block_wrapper_attributes();
-		return "<p {$attributes}>{$display_name}</p>";
+
+		$link    = $block->context['link'] ?? '';
+		$is_link = $attributes['isLink'] ?? false;
+		$rel     = $attributes['rel'] ?? '';
+
+		if ( $is_link && '' !== $link ) {
+			$inner_content = sprintf(
+				'<a rel="%s" href="%s" title="%s">%s</a>',
+				$rel,
+				$link,
+				sprintf( __( 'Posts by %s', 'co-authors-plus' ), $display_name ),
+				$display_name
+			);
+		} else {
+			$inner_content = $display_name;
+		}
+
+		return sprintf( '<p %s>%s</p>', get_block_wrapper_attributes(), $inner_content );
 	}
 }
