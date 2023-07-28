fc40fb78b53a1de12c5a391334fe3e29a1da09d2
Douglas Johnson <doug@thoughtcatalog.com>
2023-07-28 13:19:31 -0400

    build coauthors block

--- blocks/coauthors/build/index.asset.php
+++ blocks/coauthors/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd41603de0318ef9844cb');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '5a2c562ff5dd8aa1dcad');
