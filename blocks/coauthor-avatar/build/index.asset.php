820cf89bce8f1c718b548ade4ec33c8bbe7c58d7
Douglas Johnson <doug@thoughtcatalog.com>
2023-08-03 10:49:29 -0400

    default avatars to display block

--- blocks/coauthor-avatar/build/index.asset.php
+++ blocks/coauthor-avatar/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a5214c7c6f74fdbb694f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c81272de10aa20374275');
