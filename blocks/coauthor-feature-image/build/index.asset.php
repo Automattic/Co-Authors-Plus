a2b37e80b0f3871524262ae54dd8220d9b846f8d
Douglas Johnson <doug@thoughtcatalog.com>
2023-08-09 13:27:29 -0400

    build feature image block

--- blocks/coauthor-feature-image/build/index.asset.php
+++ blocks/coauthor-feature-image/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '26f13fe5d2cc650f54f6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6de75661c5837bb87cb7');
