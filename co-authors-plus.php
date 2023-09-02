7d956b9b9a92d41eaf40918341de58c4c567bc4e
Gary Jones <gary.jones@automattic.com>
2023-09-02 17:31:59 +0100

    Replace define with const syntax

--- co-authors-plus.php
+++ co-authors-plus.php
@@ -21,7 +21,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-define( 'COAUTHORS_PLUS_VERSION', '3.5.15' );
+const COAUTHORS_PLUS_VERSION = '3.5.15';
 const COAUTHORS_PLUS_FILE = __FILE__;
 
 require_once __DIR__ . '/template-tags.php';
