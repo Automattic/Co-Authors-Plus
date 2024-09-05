df1c289a15b39205371e6189916a7c271efbe55a
Oscar Keur <o.e.keur@gmail.com>
2024-09-05 13:40:09 +0200

    Move action to coauthors_update_post

--- php/class-coauthors-plus.php
+++ php/class-coauthors-plus.php
@@ -956,6 +956,8 @@ class CoAuthors_Plus {
 				}
 			}
 		}
+		do_action('cap_finished', $post_id);
+
 	}
 
 	public function has_author_terms( $post_id ): bool {
@@ -1029,7 +1031,6 @@ class CoAuthors_Plus {
 			$wpdb->update( $wpdb->posts, array( 'post_author' => $new_author->ID ), array( 'ID' => $post_id ) );
 			clean_post_cache( $post_id );
 		}
-	        do_action('coauthors_post_updated', $post_id, $coauthor_objects);
 		return true;
 
 	}
