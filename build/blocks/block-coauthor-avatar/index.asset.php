d6ae6411e86857c57120c9bfb651907f254c48e3
Gary Jones <gary.jones@automattic.com>
2025-04-15 06:42:57 +0100

    deps: Rebuild Javascript dependencies

--- build/blocks/block-coauthor-avatar/index.asset.php
+++ build/blocks/block-coauthor-avatar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b6b841ac8f86f7358245');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0f31b4e1edde6e3fbbbe');
--- build/blocks/block-coauthor-name/index.asset.php
+++ build/blocks/block-coauthor-name/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3a57c4d58ba62fabefff');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5807ee07392a2e70009a');
