8d350635f801191677ebecd01318731a88400f26
Jake <6040147+shakyjake@users.noreply.github.com>
2025-10-22 14:47:49 +0100

    Update template-tags.php
    Co-authored-by: leogermani <leogermani@gmail.com>

--- template-tags.php
+++ template-tags.php
@@ -125,7 +125,7 @@ function coauthors__echo( $tag, $type = 'tag', $separators = array(), $tag_args 
 		}
 
 		// Fallback to user_login if we get something empty
-		if ( empty( $author_text ) ) {
+		if ( empty( $author_text ) && is_object( $i->current_author ) && ! empty( $i->current_author->user_login ) ) {
 			if(!is_bool($i->current_author)){
 				$author_text = $i->current_author->user_login;
 			}
